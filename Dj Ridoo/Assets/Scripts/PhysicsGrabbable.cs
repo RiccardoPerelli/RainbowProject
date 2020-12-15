70196ec509a6222f55b24821f44c1311562fa40e
Riccardo Bumme <s277505@studenti.polit.it>
2020-12-15 09:01:21 +0100

    wip: Drag and drop sound stop and reproduction implemented

--- Dj Ridoo/Assets/Scripts/PhysicsGrabbable.cs
+++ Dj Ridoo/Assets/Scripts/PhysicsGrabbable.cs
@@ -8,24 +8,30 @@ public class PhysicsGrabbable : Grabbable
 {
     private Rigidbody _rigidbody;
     private Collider _collider;
+    private AudioSource _audioSource;
 
     protected override void Start ()
     {
         base.Start();
         _collider = GetComponent<Collider>();
         _rigidbody = GetComponent<Rigidbody>();
-
+        if(GetComponent<AudioSource>() != null)
+            _audioSource = GetComponent<AudioSource>();
     }
 
     public override void Grab(GameObject grabber)
     {
         _collider.enabled = false;
         _rigidbody.isKinematic = true;
+        if(_audioSource != null)
+            _audioSource.Pause();
     }
 
     public override void Drop()
     {
         _collider.enabled = true;
         _rigidbody.isKinematic = false;
+        if (_audioSource != null)
+            _audioSource.Play();
     }
 }
