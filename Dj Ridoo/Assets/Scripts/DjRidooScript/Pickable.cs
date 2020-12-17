f1720d71ba5178a5677c0280ee3938ccf5230fd7
Riccardo Bumme <s277505@studenti.polit.it>
2020-12-18 00:33:38 +0100

    First integration with Unity (Oculus package needed)

---
+++ Dj Ridoo/Assets/Scripts/DjRidooScript/Pickable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickable : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject objectToSpawn;
+
+    protected void Start()
+    {
+        
+    }
+
+    public GameObject spawnObject()
+    {
+        return Instantiate(objectToSpawn, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+}
---
+++ Dj Ridoo/Assets/Scripts/DjRidooScript/PlayerFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour
+{
+    [SerializeField]
+    public float yOffset = 0;
+    public float xOffset = 0;
+    public float zOffset = 0;
+    public float followVelocity = 0;
+    public Transform playerTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerTransform != null)
+        {
+            Vector3 velocity = (transform.position - playerTransform.position) / Time.deltaTime;
+            transform.position = Vector3.Lerp(transform.position, 
+                new Vector3(playerTransform.position.x + xOffset, playerTransform.position.y + yOffset, playerTransform.position.z + zOffset), Time.deltaTime * followVelocity);
+        }
+    }
+
+}
