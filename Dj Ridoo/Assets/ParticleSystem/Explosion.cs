fb432639170e3a631bd360486bab4acd3106999d
MONICA RUNGI s276979 <s276979@studenti.polito.it>
2021-01-21 14:56:56 +0100

    Added environment

---
+++ Dj Ridoo/Assets/ParticleSystem/Explosion.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    [SerializeField] float destroyTime;
+    public GameObject explosion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, destroyTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter()
+    {
+        GameObject expl = Instantiate(explosion, transform.position, Quaternion.identity) as GameObject;
+        Destroy(expl, 3); // delete the explosion after 3 seconds
+    }
+}
