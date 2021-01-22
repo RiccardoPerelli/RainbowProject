249ffa91e428d282fe8e3947cd7c218407adf12b
Riccardo Bumme <s277505@studenti.polit.it>
2021-01-22 23:49:00 +0100

    Palette modification

--- Dj Ridoo/Assets/Scripts/DjRidooScript/AlphaController.cs
+++ Dj Ridoo/Assets/Scripts/DjRidooScript/AlphaController.cs
@@ -3,23 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AlphaController : MonoBehaviour
-{
-
-    public GameObject alphaController;
-
-    private Transform controllerTransform;
-    
-
+{    
     // Start is called before the first frame update
     void Start()
     {
-        controllerTransform = alphaController.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(alphaController.transform.rotation.z > 80)
+        if(!OVRInput.Get(OVRInput.NearTouch.SecondaryIndexTrigger))
         {
             gameObject.SetActive(true);
         } else
