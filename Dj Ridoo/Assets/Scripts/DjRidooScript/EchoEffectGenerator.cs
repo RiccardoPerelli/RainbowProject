465a5754680e93120c8536baa8aa0824e3ae4a21
Riccardo Bumme <s277505@studenti.polit.it>
2021-01-24 10:42:53 +0100

    Echo effect generator and slider interactions script (todo: implement palette interactions for each filters)

--- Dj Ridoo/Assets/Scripts/DjRidooScript/EchoEffectGenerator.cs
+++ Dj Ridoo/Assets/Scripts/DjRidooScript/EchoEffectGenerator.cs
@@ -2,17 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EchoEffectGenerator : MonoBehaviour
+public class EchoEffectGenerator : EffectGenerator
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.collider.gameObject.tag == "Instrument")
+        {
+            Debug.Log("Collision Detected with the instrument");
+            applyEffect(collision);
+        }
+        else
+        {
+            Debug.Log("Collision Detected");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void applyEffect(Collision collision)
     {
-        
+        if (collision.collider.gameObject.GetComponent<AudioEchoFilter>() == null)
+        {
+            GameObject gui = Instantiate(EffectUI, collision.collider.gameObject.transform.position + offset, Quaternion.identity) as GameObject;
+            gui.GetComponent<EchoSliderInteraction>().instruments.Add(collision.collider.gameObject);
+            collision.collider.gameObject.AddComponent(typeof(AudioEchoFilter));
+            collision.collider.gameObject.GetComponent<AudioEchoFilter>().delay = gui.GetComponent<EchoSliderInteraction>().delayLevelStartingValue;
+            collision.collider.gameObject.GetComponent<AudioEchoFilter>().decayRatio = gui.GetComponent<EchoSliderInteraction>().decayRationStartingValue;
+            collision.collider.gameObject.GetComponent<AudioEchoFilter>().wetMix = gui.GetComponent<EchoSliderInteraction>().wetMixStartingValue;
+            collision.collider.gameObject.GetComponent<AudioEchoFilter>().dryMix = gui.GetComponent<EchoSliderInteraction>().dryMixStartingValue;
+            Destroy(this.gameObject, destroyTime);
+        }
+        else
+        {
+            Debug.Log("Effect already applied!");
+        }
     }
 }
--- Dj Ridoo/Assets/Scripts/DjRidooScript/EchoSliderInteraction.cs
+++ Dj Ridoo/Assets/Scripts/DjRidooScript/EchoSliderInteraction.cs
@@ -1,18 +1,83 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-public class EchoSliderInteraction : MonoBehaviour
+public class EchoSliderInteraction : SliderInteraction
 {
+
+    public TextMeshProUGUI delayLevelText;
+    public TextMeshProUGUI decayRatioLevelText;
+    public TextMeshProUGUI wetMixLevelText;
+    public TextMeshProUGUI dryMixLevelText;
+
+    public Slider delaySlider;
+    public Slider decayRatioSlider;
+    public Slider wetMixSlider;
+    public Slider dryMixSlider;
+
+    public const int DELAY_LEVEL_MAX = 5000;
+    public const float DECAY_RATIO_MAX = 1f;
+    public const float WET_MIX_MAX = 1f;
+    public const float DRY_MIX_MAX = 1f;
+
+    public int delayLevelStartingValue = 500;
+    public float decayRationStartingValue = 0.5f;
+    public float wetMixStartingValue = 1f;
+    public float dryMixStartingValue = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //initialize the value of the sliders
+        delayLevelText.text = "Livello delay: " + delayLevelStartingValue.ToString();
+        decayRatioLevelText.text = "Decay ratio: " + decayRationStartingValue.ToString();
+        wetMixLevelText.text = "Wet mix: " + wetMixStartingValue.ToString();
+        dryMixLevelText.text = "Dry mix: " + dryMixStartingValue.ToString();
+
+        delaySlider.value = delayLevelStartingValue / DELAY_LEVEL_MAX;
+        decayRatioSlider.value = decayRationStartingValue / DECAY_RATIO_MAX;
+        wetMixSlider.value = wetMixStartingValue / WET_MIX_MAX;
+        dryMixSlider.value = dryMixStartingValue / DRY_MIX_MAX;
+
+        //Adds a listener to the main sliders and invokes a method when the value changes.
+        delaySlider.onValueChanged.AddListener(OnDelaySliderValueChanged);
+        decayRatioSlider.onValueChanged.AddListener(OnDecayRatioSliderValueChanged);
+        wetMixSlider.onValueChanged.AddListener(OnWetMixSliderValueChanged);
+        dryMixSlider.onValueChanged.AddListener(OnDryMixSliderValueChanged);
     }
 
-    // Update is called once per frame
-    void Update()
+    protected void OnDelaySliderValueChanged(float value)
     {
-        
+        foreach (var instrument in instruments)
+        {
+            instrument.GetComponent<AudioEchoFilter>().delay = value * DELAY_LEVEL_MAX;
+            delayLevelText.text = "Livello delay: " + instrument.GetComponent<AudioEchoFilter>().delay.ToString();
+        }
+    }
+    protected void OnDecayRatioSliderValueChanged(float value)
+    {
+        foreach (var instrument in instruments)
+        {
+            instrument.GetComponent<AudioEchoFilter>().decayRatio = value * DECAY_RATIO_MAX;
+            decayRatioLevelText.text = "Livello distorsione: " + instrument.GetComponent<AudioEchoFilter>().decayRatio.ToString();
+        }
+    }
+    protected void OnWetMixSliderValueChanged(float value)
+    {
+        foreach (var instrument in instruments)
+        {
+            instrument.GetComponent<AudioEchoFilter>().wetMix = value * WET_MIX_MAX;
+            wetMixLevelText.text = "Wet mix: " + instrument.GetComponent<AudioEchoFilter>().wetMix.ToString();
+        }
+    }
+    protected void OnDryMixSliderValueChanged(float value)
+    {
+        foreach (var instrument in instruments)
+        {
+            instrument.GetComponent<AudioEchoFilter>().dryMix = value * DRY_MIX_MAX;
+            dryMixLevelText.text = "Dry mix: " + instrument.GetComponent<AudioEchoFilter>().dryMix.ToString();
+        }
     }
 }
