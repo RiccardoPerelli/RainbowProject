b342c3713585a27b93081b0edea5951014eb30e5
Monica Rungi <48438059+MonicaRungi@users.noreply.github.com>
2021-02-05 20:34:56 +0100

    Floating and Activation Palette

---
+++ Dj Ridoo/Assets/Scripts/DjRidooScript/FloatingUpandDown.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingUpandDown : MonoBehaviour
+{
+    public float amplitude = 0.1f;
+    public float frequency = 0.5f;
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
+    bool floatup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        posOffset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPos;
+    }
+}
---
+++ Dj Ridoo/Assets/Scripts/DjRidooScript/PaletteActivation.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaletteActivation : MonoBehaviour
+{
+    private Animator _animator;
+    bool activated;
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        activated = false;
+    }
+
+    
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.Two)){
+            if (!activated)
+            {
+                Activate();
+                activated = true;
+            }
+            else
+                Deactivate();
+        }
+    }
+
+    public void Activate()
+    {
+        if (_animator == null)
+            return;
+        _animator.SetBool("Active", true);
+    }
+
+    public void Deactivate()
+    {
+        if (_animator == null)
+            return;
+        _animator.SetBool("Active", false);
+    }
+}
