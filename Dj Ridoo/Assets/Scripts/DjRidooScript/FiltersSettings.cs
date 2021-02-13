a1b96515e88d0adb68a83c06f2131e061fb2b8b1
Riccardo Bumme <s277505@studenti.polit.it>
2021-02-13 19:39:59 +0100

    saving merge pt.1

--- Dj Ridoo/Assets/Scripts/DjRidooScript/FiltersSettings.cs
+++ Dj Ridoo/Assets/Scripts/DjRidooScript/FiltersSettings.cs
@@ -90,7 +90,8 @@ public class FiltersSettings : MonoBehaviour
                         LowPass lp = JsonUtility.FromJson<LowPass>(p);
                         float cutofffreq = lp.cutoff_freq;
                         float resonance = lp.resonance;
-
+                        lowpass.transform.Find("FrequencySlider").position = new Vector3(lowpass.transform.Find("FrequencySlider").gameObject.GetComponent<SliderPosition>().startingXPosition,
+                            cutofffreq, lowpass.transform.Find("FrequencySlider").gameObject.GetComponent<SliderPosition>().startingZPosition);
 
                     }
                     
