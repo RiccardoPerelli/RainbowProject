14ab79720e434f85d77f21ad9ae6ca89161b79ce
Monica Rungi <48438059+MonicaRungi@users.noreply.github.com>
2021-02-17 15:36:23 +0100

    CommandMap + VideoButton

---
+++ Dj Ridoo/Assets/Scripts/DjRidooScript/CommandMap.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandMap : MonoBehaviour
+{
+    private float delay = 2f;
+    private Animator _animator;
+    private AudioSource audioSource;
+
+    public GameObject user;
+
+    private bool check = false;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_animator == null)
+            return;
+
+        if(user.transform.position.x <= 2.5f && user.transform.position.z <= 2.5f)
+        {
+            _animator.SetBool("Open", true);
+            if (check == false)
+            {
+                audioSource.Play();
+                check = true;
+            }
+        }
+        else
+        {
+            _animator.SetBool("Open", false);
+            if(check == true)
+            {
+                audioSource.Play();
+                check = false;
+            }
+        }
+    }
+}
