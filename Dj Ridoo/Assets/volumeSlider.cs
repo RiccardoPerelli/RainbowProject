30bd42cdbe39eb9f1d660e8de49fd8fc11ce4019
Monica Rungi <48438059+MonicaRungi@users.noreply.github.com>
2021-02-17 23:24:18 +0100

    Volume added

---
+++ Dj Ridoo/Assets/volumeSlider.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class volumeSlider : MonoBehaviour
+{
+	public TextMeshProUGUI sliderValueText;
+
+	public Slider slider;
+
+	private Transform parentObj;
+
+	public void Start()
+	{
+		sliderValueText.text = slider.value.ToString();
+
+		//Adds a listener to the main slider and invokes a method when the value changes.
+		slider.onValueChanged.AddListener(OnSliderValueChanged);
+	}
+
+	public void OnSliderValueChanged(float value)
+	{
+		sliderValueText.text = value.ToString();
+		parentObj = gameObject.transform.parent;
+		parentObj.gameObject.GetComponent<AudioSource>().volume = value;
+	}
+
+}
