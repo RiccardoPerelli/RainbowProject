7dde1abb696cc89542a8b9123463c76963b0bd5d
Monica Rungi <48438059+MonicaRungi@users.noreply.github.com>
2021-02-18 13:28:54 +0100

    Haptic feedback & Cable shader

---
+++ Dj Ridoo/Assets/Scripts/DjRidooScript/Haptics.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Haptics : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider coll)
+    {
+        if(coll.gameObject.tag == "Essence")
+        {
+            vibrateHand(0.3f);
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "Instrument")
+        {
+            vibrateHand(1);
+        }
+
+        if (coll.gameObject.tag == "Filter")
+        {
+            vibrateHand(0.5f);
+        }
+
+        if(coll.gameObject.tag == "Slider")
+        {
+            vibrateHand(0.3f);
+        }
+    }
+
+    private void vibrateHand(float amplitude)
+    {
+        if (gameObject.tag == "LHand")
+            OVRInput.SetControllerVibration(1, amplitude, OVRInput.Controller.LTouch);
+        if (gameObject.tag == "RHand")
+            OVRInput.SetControllerVibration(1, amplitude, OVRInput.Controller.RTouch);
+        print("vibrazione!");
+    }
+}
